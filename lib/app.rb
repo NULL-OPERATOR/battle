dd6cfd4fb107bc5c8b15040f85d56ed335af62f7
hasulica <raduasandei@gmail.com>
2015-10-28 10:56:29 +0000

    Implemented first use story

--- lib/app.rb
+++ lib/app.rb
@@ -2,9 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb(:index)
   end
 
+  post '/names' do
+    @player_one_name = params[:player_one_name]
+    @player_two_name = params[:player_two_name]
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
