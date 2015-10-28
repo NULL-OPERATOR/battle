71cb12593fd03c7bf00484bd31ea06eb11a2a3ed
hasulica <raduasandei@gmail.com>
2015-10-28 16:06:29 +0000

    Added hitpoint test for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,13 @@ require 'player'
 describe Player do
 
   subject(:raedgar) { described_class.new('Raedgar') }
+
   it 'returns a player\'s name' do
     expect(raedgar.name).to eq 'Raedgar'
   end
+
+  it 'has hit points' do
+    expect(raedgar.hit_points).to eq 60
+  end
+
 end
